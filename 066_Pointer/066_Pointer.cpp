c6c4b358650333c40bafc1a2e8171d8988731953
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-05-10 10:59:00 +0700

    Pointer Reference refMhs

--- 066_Pointer/066_Pointer.cpp
+++ 066_Pointer/066_Pointer.cpp
@@ -17,7 +17,7 @@ int main()
 	mahasiswa mhs{ 1 };		//object mhs 
 	mhs.showNim();			//Member Acces Operator 
 
-	mahasiswa ref = mhs;	//Pointer Reference refMhs
+	mahasiswa ref = mhs;	//Pointer Reference refMhs 
 	ref.nim = 2;			//Member Access Operator
 	mhs.showNim();
 
