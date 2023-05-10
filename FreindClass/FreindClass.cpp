c9832cc45b24a66f709b95fad2c5e45f29fdc088
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-05-10 11:34:32 +0700

    menentukkan void orang terhadap setNama

--- FreindClass/FreindClass.cpp
+++ FreindClass/FreindClass.cpp
@@ -25,5 +25,21 @@ void siswa::displayAll(orang& a) {
 }
 
 void siswa::setId(int pId) {
+	id = pId;
+}
 
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+int main()
+{
+	orang o;
+	o.setNama("  Joko Kumat");
+	siswa s;
+	s.setId(1);
+	s.displayAll(o);
+	cout << endl;
+	system(" pause");
+	return 0;
 }
